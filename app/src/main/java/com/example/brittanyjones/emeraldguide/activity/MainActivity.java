0234c0a963ca4b4712577b85189c90f0fc6f2171
Brittany Jones <brittany.r.jones@hotmail.com>
2018-07-08 00:03:09 -0700

    restructured files, all activities in one folder

--- app/src/main/java/com/example/brittanyjones/emeraldguide/activity/MainActivity.java
+++ app/src/main/java/com/example/brittanyjones/emeraldguide/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.brittanyjones.emeraldguide;
+package com.example.brittanyjones.emeraldguide.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.brittanyjones.emeraldguide.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/brittanyjones/emeraldguide/activity/ShowerActivity.java
+++ app/src/main/java/com/example/brittanyjones/emeraldguide/activity/ShowerActivity.java
@@ -1,7 +1,9 @@
-package com.example.brittanyjones.emeraldguide;
+package com.example.brittanyjones.emeraldguide.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.brittanyjones.emeraldguide.R;
 
 public class ShowerActivity extends AppCompatActivity {
 
