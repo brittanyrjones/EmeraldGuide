c0a8196d119c90cdbfaf8909f8531d856f80b03b
Brittany Jones <brittany.r.jones@hotmail.com>
2018-07-09 09:39:13 -0700

    declare and initialise Retrofit in the constructor of NetworkService.

---
+++ app/src/main/java/com/example/brittanyjones/emeraldguide/activity/network/NetworkService.java
@@ -0,0 +1,25 @@
+package com.example.brittanyjones.emeraldguide.activity.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkService {
+
+    private static NetworkService mInstance;
+    private static final String BASE_URL = "https://data.seattle.gov";
+    private Retrofit mRetrofit;
+
+    private NetworkService() {
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static NetworkService getInstance() {
+        if (mInstance == null) {
+            mInstance = new NetworkService();
+        }
+        return mInstance;
+    }
+}
