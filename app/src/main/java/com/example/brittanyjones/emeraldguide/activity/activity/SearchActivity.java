cb0588b513c44f31228fc213452650b4ae461f13
Brittany Jones <brittany.r.jones@hotmail.com>
2018-07-10 23:29:57 -0700

    added initial file setup

--- app/src/main/java/com/example/brittanyjones/emeraldguide/activity/activity/SearchActivity.java
+++ app/src/main/java/com/example/brittanyjones/emeraldguide/activity/activity/SearchActivity.java
@@ -1,12 +1,11 @@
 package com.example.brittanyjones.emeraldguide.activity.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.brittanyjones.emeraldguide.R;
 
 public class SearchActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
