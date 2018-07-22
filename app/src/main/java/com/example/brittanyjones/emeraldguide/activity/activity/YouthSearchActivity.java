453d121617dfb81d30bf947a0f40460c93369525
Brittany Jones <brittany.r.jones@hotmail.com>
2018-07-21 18:20:33 -0700

    dummy data for youth now working

--- app/src/main/java/com/example/brittanyjones/emeraldguide/activity/activity/YouthSearchActivity.java
+++ app/src/main/java/com/example/brittanyjones/emeraldguide/activity/activity/YouthSearchActivity.java
@@ -1,28 +1,29 @@
 package com.example.brittanyjones.emeraldguide.activity.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ListView;
 
 import com.example.brittanyjones.emeraldguide.R;
-import com.example.brittanyjones.emeraldguide.activity.adapter.ResourceAdapter;
+import com.example.brittanyjones.emeraldguide.activity.model.Feed;
+import com.example.brittanyjones.emeraldguide.activity.model.Info;
+import com.mindorks.placeholderview.ExpandablePlaceHolderView;
+
+import java.util.Objects;
 
 public class YouthSearchActivity extends AppCompatActivity {
-
-
-
-    private final static String JSON_FILE_ANDROID_YOUTHWEAR = "youthresource.json";
-    private final static String TAG = "ResourceActivity";
-
-    private ResourceAdapter resourceAdapter;
-    private ListView listView;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youth_search);
+        setContentView(R.layout.activity_search);
+        Context mContext = this.getApplicationContext();
+        ExpandablePlaceHolderView mExpandableView = (ExpandablePlaceHolderView) findViewById(R.id.expandableView);
+        for(Feed feed : Objects.requireNonNull(Utils.loadFeeds(this.getApplicationContext()))){
+            mExpandableView.addView(new HeadingView(mContext, feed.getHeading()));
+            for(Info info : feed.getInfoList()){
+                mExpandableView.addView(new InfoView(mContext, info));
+            }
+        }
     }
 }
